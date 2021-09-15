7b10674fd902192d6afad21a9d8e66e4cca4b483
Rishabh Bohra <rishabhbohra01@gmail.com>
2021-09-16 04:55:55 +0530

    feat: Add diagnostics cmd

---
+++ cmd/root.go
@@ -0,0 +1,88 @@
+package cmd
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+
+	troubleshootv1beta2 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta2"
+	"github.com/replicatedhq/troubleshoot/pkg/k8sutil"
+	"github.com/replicatedhq/troubleshoot/pkg/logger"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+func RootCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "support-bundle [url]",
+		Args:  cobra.MinimumNArgs(1),
+		Short: "Generate a support bundle",
+		Long: `A support bundle is an archive of files, output, metrics and state
+from a server that can be used to assist when troubleshooting a Kubernetes cluster.`,
+		SilenceUsage: true,
+		PreRun: func(cmd *cobra.Command, args []string) {
+			viper.BindPFlags(cmd.Flags())
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			v := viper.GetViper()
+
+			logger.SetQuiet(v.GetBool("quiet"))
+			return runTroubleshoot(v, args[0])
+		},
+	}
+
+	cobra.OnInitialize(initConfig)
+
+	//cmd.AddCommand(Analyze())
+	//cmd.AddCommand(VersionCmd())
+
+	cmd.Flags().StringSlice("redactors", []string{}, "names of the additional redactors to use")
+	cmd.Flags().Bool("redact", true, "enable/disable default redactions")
+	cmd.Flags().Bool("collect-without-permissions", true, "always generate a support bundle, even if it some require additional permissions")
+	cmd.Flags().String("since-time", "", "force pod logs collectors to return logs after a specific date (RFC3339)")
+	cmd.Flags().String("since", "", "force pod logs collectors to return logs newer than a relative duration like 5s, 2m, or 3h.")
+
+	// hidden in favor of the `insecure-skip-tls-verify` flag
+	cmd.Flags().Bool("allow-insecure-connections", false, "when set, do not verify TLS certs when retrieving spec and reporting results")
+	cmd.Flags().MarkHidden("allow-insecure-connections")
+
+	viper.BindPFlags(cmd.Flags())
+
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+
+	k8sutil.AddFlags(cmd.Flags())
+
+	return cmd
+}
+
+func InitAndExecute() {
+	if err := RootCmd().Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func initConfig() {
+	viper.SetEnvPrefix("TROUBLESHOOT")
+	viper.AutomaticEnv()
+}
+
+func ensureCollectorInList(list []*troubleshootv1beta2.Collect, collector troubleshootv1beta2.Collect) []*troubleshootv1beta2.Collect {
+	for _, inList := range list {
+		if collector.ClusterResources != nil && inList.ClusterResources != nil {
+			return list
+		}
+		if collector.ClusterInfo != nil && inList.ClusterInfo != nil {
+			return list
+		}
+	}
+
+	return append(list, &collector)
+}
+
+func writeFile(filename string, contents []byte) error {
+	if err := ioutil.WriteFile(filename, contents, 0644); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ cmd/run.go
@@ -0,0 +1,242 @@
+package cmd
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"net/url"
+	"path/filepath"
+	"strings"
+	"time"
+
+	cursor "github.com/ahmetb/go-cursor"
+	"github.com/fatih/color"
+	"github.com/pkg/errors"
+	troubleshootv1beta2 "github.com/replicatedhq/troubleshoot/pkg/apis/troubleshoot/v1beta2"
+	"github.com/replicatedhq/troubleshoot/pkg/client/troubleshootclientset/scheme"
+	troubleshootclientsetscheme "github.com/replicatedhq/troubleshoot/pkg/client/troubleshootclientset/scheme"
+	"github.com/replicatedhq/troubleshoot/pkg/convert"
+	"github.com/replicatedhq/troubleshoot/pkg/docrewrite"
+	"github.com/replicatedhq/troubleshoot/pkg/httputil"
+	"github.com/replicatedhq/troubleshoot/pkg/k8sutil"
+	"github.com/replicatedhq/troubleshoot/pkg/supportbundle"
+	"github.com/spf13/viper"
+	spin "github.com/tj/go-spin"
+)
+
+func runTroubleshoot(v *viper.Viper, arg string) error {
+	restConfig, err := k8sutil.GetRESTConfig()
+	if err != nil {
+		return errors.Wrap(err, "failed to convert kube flags to rest config")
+	}
+
+	var sinceTime *time.Time
+	if v.GetString("since-time") != "" || v.GetString("since") != "" {
+		sinceTime, err = parseTimeFlags(v)
+		if err != nil {
+			return errors.Wrap(err, "failed parse since time")
+		}
+	}
+
+	if v.GetBool("allow-insecure-connections") || v.GetBool("insecure-skip-tls-verify") {
+		httputil.AddTransport(&http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		})
+	}
+
+	collectorContent, err := supportbundle.LoadSupportBundleSpec(arg)
+	if err != nil {
+		return errors.Wrap(err, "failed to load collector spec")
+	}
+
+	multidocs := strings.Split(string(collectorContent), "\n---\n")
+
+	// we support both raw collector kinds and supportbundle kinds here
+	supportBundle, err := supportbundle.ParseSupportBundleFromDoc([]byte(multidocs[0]))
+	if err != nil {
+		return errors.Wrap(err, "failed to parse collector")
+	}
+
+	troubleshootclientsetscheme.AddToScheme(scheme.Scheme)
+	decode := scheme.Codecs.UniversalDeserializer().Decode
+
+	additionalRedactors := &troubleshootv1beta2.Redactor{}
+	for idx, redactor := range v.GetStringSlice("redactors") {
+		redactorObj, err := supportbundle.GetRedactorFromURI(redactor)
+		if err != nil {
+			return errors.Wrapf(err, "failed to get redactor spec %s, #%d", redactor, idx)
+		}
+
+		if redactorObj != nil {
+			additionalRedactors.Spec.Redactors = append(additionalRedactors.Spec.Redactors, redactorObj.Spec.Redactors...)
+		}
+	}
+
+	for i, additionalDoc := range multidocs {
+		if i == 0 {
+			continue
+		}
+		additionalDoc, err := docrewrite.ConvertToV1Beta2([]byte(additionalDoc))
+		if err != nil {
+			return errors.Wrap(err, "failed to convert to v1beta2")
+		}
+		obj, _, err := decode(additionalDoc, nil, nil)
+		if err != nil {
+			return errors.Wrapf(err, "failed to parse additional doc %d", i)
+		}
+		multidocRedactors, ok := obj.(*troubleshootv1beta2.Redactor)
+		if !ok {
+			continue
+		}
+		additionalRedactors.Spec.Redactors = append(additionalRedactors.Spec.Redactors, multidocRedactors.Spec.Redactors...)
+	}
+
+	s := spin.New()
+	finishedCh := make(chan bool, 1)
+	progressChan := make(chan interface{}) // non-zero buffer can result in missed messages
+	isFinishedChClosed := false
+	go func() {
+		currentDir := ""
+		for {
+			select {
+			case msg := <-progressChan:
+				switch msg := msg.(type) {
+				case error:
+					c := color.New(color.FgHiRed)
+					c.Println(fmt.Sprintf("%s\r * %v", cursor.ClearEntireLine(), msg))
+				case string:
+					currentDir = filepath.Base(msg)
+				}
+			case <-finishedCh:
+				fmt.Printf("\r%s\r", cursor.ClearEntireLine())
+				return
+			case <-time.After(time.Millisecond * 100):
+				if currentDir == "" {
+					fmt.Printf("\r%s \033[36mCollecting support bundle\033[m %s", cursor.ClearEntireLine(), s.Next())
+				} else {
+					fmt.Printf("\r%s \033[36mCollecting support bundle\033[m %s %s", cursor.ClearEntireLine(), s.Next(), currentDir)
+				}
+			}
+		}
+	}()
+	defer func() {
+		if !isFinishedChClosed {
+			close(finishedCh)
+		}
+	}()
+
+	collectorCB := func(c chan interface{}, msg string) {
+		c <- fmt.Sprintf("%s", msg)
+	}
+
+	createOpts := supportbundle.SupportBundleCreateOpts{
+		CollectorProgressCallback: collectorCB,
+		CollectWithoutPermissions: v.GetBool("collect-without-permissions"),
+		KubernetesRestConfig:      restConfig,
+		Namespace:                 v.GetString("namespace"),
+		ProgressChan:              progressChan,
+		SinceTime:                 sinceTime,
+		FromCLI:                   true,
+	}
+
+	c := color.New()
+	c.Println(fmt.Sprintf("\r%s\r", cursor.ClearEntireLine()))
+
+	response, err := supportbundle.CollectSupportBundleFromSpec(&supportBundle.Spec, additionalRedactors, createOpts)
+	if err != nil {
+		return errors.Wrap(err, "failed to run collect and analyze process")
+	}
+
+	if len(response.AnalyzerResults) > 0 {
+
+		data := convert.FromAnalyzerResult(response.AnalyzerResults)
+		formatted, err := json.MarshalIndent(data, "", "    ")
+		if err != nil {
+			c := color.New(color.FgHiRed)
+			c.Printf("%s\r * Failed to format analysis: %v\n", cursor.ClearEntireLine(), err)
+		}
+
+		fmt.Printf("%s", formatted)
+	}
+
+	if !response.FileUploaded {
+		msg := response.ArchivePath
+		if appName := supportBundle.Labels["applicationName"]; appName != "" {
+			f := `A support bundle for %s has been created in this directory
+named %s. Please upload it on the Troubleshoot page of
+the %s Admin Console to begin analysis.`
+			msg = fmt.Sprintf(f, appName, response.ArchivePath, appName)
+		}
+
+		fmt.Printf("%s\n", msg)
+
+		return nil
+	}
+
+	fmt.Printf("\r%s\r", cursor.ClearEntireLine())
+	if response.FileUploaded {
+		fmt.Printf("A support bundle has been created and uploaded to your cluster for analysis. Please visit the Troubleshoot page to continue.\n")
+		fmt.Printf("A copy of this support bundle was written to the current directory, named %q\n", response.ArchivePath)
+	} else {
+		fmt.Printf("A support bundle has been created in the current directory named %q\n", response.ArchivePath)
+	}
+	return nil
+}
+
+func getExpectedContentType(uploadURL string) string {
+	parsedURL, err := url.Parse(uploadURL)
+	if err != nil {
+		return ""
+	}
+	return parsedURL.Query().Get("Content-Type")
+}
+
+func parseTimeFlags(v *viper.Viper) (*time.Time, error) {
+	var (
+		sinceTime time.Time
+		err       error
+	)
+	if v.GetString("since-time") != "" {
+		if v.GetString("since") != "" {
+			return nil, errors.Errorf("at most one of `sinceTime` or `since` may be specified")
+		}
+		sinceTime, err = time.Parse(time.RFC3339, v.GetString("since-time"))
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to parse --since-time flag")
+		}
+	} else {
+		parsedDuration, err := time.ParseDuration(v.GetString("since"))
+		if err != nil {
+			return nil, errors.Wrap(err, "unable to parse --since flag")
+		}
+		now := time.Now()
+		sinceTime = now.Add(0 - parsedDuration)
+	}
+
+	return &sinceTime, nil
+}
+
+func shouldRetryRequest(err error) bool {
+	if strings.Contains(err.Error(), "x509") {
+		httputil.AddTransport(&http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		})
+		return true
+	}
+	return false
+}
+
+// func canTryInsecure() bool {
+// 	if !isatty.IsTerminal(os.Stdout.Fd()) {
+// 		return false
+// 	}
+// 	prompt := promptui.Prompt{
+// 		Label:     "Connection appears to be insecure. Would you like to attempt to create a support bundle anyway?",
+// 		IsConfirm: true,
+// 	}
+
+// 	_, err := prompt.Run()
+// 	return err == nil
+// }
